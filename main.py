98e7c25ec6a0ed7f285e147cde85fad04310c234
samuel <osenseijedi@gmail.com>
2026-04-18 16:25:49 +0200

    General Cleanup

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 import argparse
 import serial
 from serial.tools.list_ports import comports
-import pygame
 import time
+import os
+import threading
 
-# Initialize the pygame mixer
-pygame.mixer.init()
-import os;
+# Remove pygame startup message. To do before importing pygame.
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+import pygame
+
+
+def playSound(wav_file):
+    sound = pygame.mixer.Sound(wav_file)
+
+    sound.play()
+    time.sleep(sound.get_length())
 
 
 def main(command: str):
-    if command != 'aiDone' and command != 'aiQuestion':
+    if command not in ['aiDone', 'aiQuestion']:
         print("Invalid command. Please use 'aiDone' or 'aiQuestion'.")
         return
 
+    # Initialize the pygame mixer
+    pygame.mixer.init()
+
     ports = comports()
+
+    script_path = os.path.abspath(os.path.dirname(__file__))
+    t = threading.Thread(target=playSound, args=(f'{script_path}/{command}_2.wav',))
+
+    t.start()
 
     for port in ports:
         if port.description.startswith('Arduino Micro'):
@@ -32,17 +48,12 @@ def main(command: str):
                 data = command.encode()
                 ser.write(data)
                 ser.flush()
-                script_path = os.path.abspath(os.path.dirname(__file__))
-
-                sound = pygame.mixer.Sound(f'{script_path}/{command}_2.wav')
-
-                sound.play()
-                time.sleep(sound.get_length())
 
             except Exception as e:
                 print(f"Error: {e}")
             finally:
                 ser.close()
+    t.join()
 
 
 if __name__ == '__main__':
